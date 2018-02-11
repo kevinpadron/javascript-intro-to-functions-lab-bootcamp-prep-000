7cd791a53656f4067fc133d8c6cb7893f6708df6
Kevin Padron <kvnnpdrn@gmail.com>
2018-02-11 01:56:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,16 @@ function logWhisper(string) {
 //     return "I love you, too."
 //   }
 // }
+
+function sayHiToGrandma(string) {
+  // var lowercase = string
+  // var mixedcase = string
+  // var uppercase = string
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  } else if (string.toUpperCase() === string){
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
