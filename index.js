31ea9ffd6a83fd8701f9800bfcc9dee5eeab43f0
Kevin Padron <kvnnpdrn@gmail.com>
2018-02-11 01:43:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = string
   if (lowercase.toLowerCase() === lowercase) {
     return "I can't hear you!"
   } else {
