fdb0dc570f1e4de1dd44132a3ee1269cb00fc5a1
Kevin Padron <kvnnpdrn@gmail.com>
2018-02-11 01:48:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (uppercase.toUpperCase() === uppercase){
     return "YES INDEED!"
-  } else if (mixedcase.toUpperCase() === mixedcase) {
+  } else {
     return "I love you, too."
   }
 }
